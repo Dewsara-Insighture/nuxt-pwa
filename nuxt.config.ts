1abdef98536eaad05530d9c3fe2806c307821995
Dewsara-Insighture <dewsara.wijerathne@insighture.com>
2025-01-07 15:43:26 +0530

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,54 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@vite-pwa/nuxt'],
+   // PWA module configuration
+   pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Nuxt PWA App',
+      short_name: 'NuxtPWA',
+      description: 'A simple Nuxt PWA example',
+      theme_color: '#4DBA87',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+       icons: [
+        { src: '/pwa-assets/pwa-64x64.png', sizes: '64x64', type: 'image/png' },
+        {
+          src: '/pwa-assets/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/pwa-assets/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: '/pwa-assets/maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+      related_applications: [
+        {
+          platform: 'leader',
+          url: 'https://dev.leaderexpenses-intrepidtravel-com.intrepidtravel.cloud/manifest.json',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      type: 'module'
+    }
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h1>Welcome to Nuxt PWA App</h1>
+    <p>This is a simple PWA using Nuxt 3 and the PWA module.</p>
+    <button @click="handlePWANavigate" class="button">Go to Leader PWA</button>
+    <a href="https://dev.leaderexpenses-intrepidtravel-com.intrepidtravel.cloud" target="_blank" rel="noopener noreferrer" class="aTag">
+  Leader PWA A link
+</a>
+  
+  </div>
+</template>
+
+<script setup lang="ts">
+function handlePWANavigate() {
+  window.location.href = 'https://dev.leaderexpenses-intrepidtravel-com.intrepidtravel.cloud';
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 50px;
+}
+.button {
+  margin-top: 20px;
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #4dba87;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+}
+
+.aTag {
+    margin :0px 10px;
+    background-color: #443a87;
+    padding: 10px 20px;
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+}
+</style>
